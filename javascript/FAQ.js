36668d9936c33e8ffb2e0b9bbea766329feca7e0
DasRL <telrpll1317@gmail.com>
2023-01-12 16:52:42 +0100

    Modif Faq / Formul Question

--- javascript/FAQ.js
+++ javascript/FAQ.js
@@ -1,5 +1,6 @@
-function myFunction() {
-  var x = document.getElementById("myDIV1");
+function myFunction(id) {
+  const div = ["myDIV1","myDIV2","myDIV3","myDIV4","myDIV5"]
+  var x = document.getElementById(div[id]);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
