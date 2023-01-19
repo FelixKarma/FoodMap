515d33440b75d45bdc5b387dbf4c5b64f13b753e
LucieBrtt <lucie.bertaut@gmail.com>
2023-01-19 12:02:28 +0100

    modifications js fiche recette

--- javascript/page_recette.js
+++ javascript/page_recette.js
@@ -13,6 +13,8 @@ function fillHeart() {
 window.onload = () => {
     const stars = document.querySelectorAll(".fi-rs-star"); // on récupère les étoiles
 
+    const note = document.querySelector("#note");
+
     for(star of stars){
         star.addEventListener("mouseover", function(){
             resetStars();
@@ -28,10 +30,33 @@ window.onload = () => {
         })
     };
 
-    function resetStars(){
+    function resetStars(note = 0){
         for(star of stars){
+            if(star.dataset.value > note){
             star.classList.add("fi-rs-star");
             star.classList.remove("fi-ss-star");
+            
+            }
+            else {
+            star.classList.add("fi-ss-star");
+            star.classList.remove("fi-rs-star");
+            }
         }
     }
+
+    //écoute du clic
+    star.addEventListener("click", function(){
+        note.value = this.dataset.value;
+    });
+
+    star.addEventListener("mouseout", function(){
+        resetStars(note.value);
+    });
 };
+
+//affichage moyenne
+
+/*let moy = document.getElementById("moyenne");
+let myMoy = document.createElement('p');
+myMoy.innerText = "Moy."+note.value;
+moy.append(myMoy);*/
