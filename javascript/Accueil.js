9fe959fe07635f83bc00f057bb0a2ee9ebda9a84
DasRL <telrpll1317@gmail.com>
2023-01-20 09:28:16 +0100

    Modif FAQ / Formu Quest / Accueil

--- javascript/Accueil.js
+++ javascript/Accueil.js
@@ -32,86 +32,117 @@ rightArrow.addEventListener('click', function () {
 
 
 
-//                                              Essai n°1
+// //                                              Essai n°1
 
 
+
+// // let search = document.getElementById("search");
+
+
+// // function UrlExists(url) {
+// //     var http = new XMLHttpRequest();
+// //     http.open('HEAD', url, false);
+// //     http.send();
+// //     if (http.status != 404)
+// //         return true;
+// //     else
+// //         return false;
+// // }
+
+// // search.addEventListener("keypress", function (event) {
+// //     var a = document.getElementById("search").value;
+    
+// //     if (event.key === "Enter") {
+// //         var url = document.location.assign("recette " + a + ".html");
+// //         if (a !== null && a !== "") {
+// //             if(UrlExists(url) == true){
+// //                 document.location.assign("recette " + a + ".html");
+// //             } else {
+// //                 document.location.href = "404.html";
+// //             } 
+// //         }
+// //     }
+// // })
+
+
+
+// //                                                      Essai n°2
+
+
+
+// // Index recettes
+
+// let recettes = ["falafel", "fricassé", "tiramisu"];
+
+// // Annonce variables i = 1 valeur du tableau, recherche = mot entré (id "search" de la barre de recherche), reponse = resultat si i = recherche
+
+// let i, recherche, reponse
+
+// // Récupération du mot recherché
+
+// recherche = document.getElementById("search").value.toUpperCase();
+
+// // i = 1 des éléments du tableau
+
+// i = document.getElementById("recettes");
+
+// i = recettes.getElementByTagName("");
+
+
+
+// //                                                     Essai n°3
+
+
+
+
+// // Index recettes
+
+// const recettes = ["falafel", "fricassé", "tiramisu"];
+
+
+
+// const reponse = recettes.filter(recommandation => recommandation.value == "search");
+
+
+
+// //                                                  Essai final (exo) ? 
+
+
+
+// // Index recettes
+
+// let recettes = ["falafel", "fricassé", "tiramisu"];
+
+// // Récupération du mot recherché
 
 // let search = document.getElementById("search");
 
+// search.addEventListener("keydown", function (event) {
 
-// function UrlExists(url) {
-//     var http = new XMLHttpRequest();
-//     http.open('HEAD', url, false);
-//     http.send();
-//     if (http.status != 404)
-//         return true;
-//     else
-//         return false;
-// }
+//     // Recherche = mot entré (id "search" de la barre de recherche), reponse = resultat
+//     let recherche = document.getElementById("search").value.toLowerCase();
+//     let reponse;
 
-// search.addEventListener("keypress", function (event) {
-//     var a = document.getElementById("search").value;
+//     //Detection de la touche entrée
+   
+//         //Parcours le tableau recettes grace à for of
+//             //On compare la recherche avec les différents éléments du tableau
+
+//                 //On redirige vers la page recherchée si trouvée
+
+//                 //On passe reponse à true pour dire que c'est bon
+                
+//                 //On break pour casser la boucle for, au cas où on a plusieurs éléments dans le tableau après le resultat trouvé
+               
+//         //On check enfin si la reponse est false pour renvoyer vers 404
+        
     
-//     if (event.key === "Enter") {
-//         var url = document.location.assign("recette " + a + ".html");
-//         if (a !== null && a !== "") {
-//             if(UrlExists(url) == true){
-//                 document.location.assign("recette " + a + ".html");
-//             } else {
-//                 document.location.href = "404.html";
-//             } 
-//         }
-//     }
 // })
 
 
-
-//                                                      Essai n°2
-
-
-
 // Index recettes
 
-let recettes = ["falafel", "fricassé", "tiramisu"];
-
-// Annonce variables i = 1 valeur du tableau, recherche = mot entré (id "search" de la barre de recherche), reponse = resultat si i = recherche
-
-let i, recherche, reponse
-
-// Récupération du mot recherché
-
-recherche = document.getElementById("search").value.toUpperCase();
-
-// i = 1 des éléments du tableau
-
-i = document.getElementById("recettes");
-
-i = recettes.getElementByTagName("");
-
-
-
-//                                                     Essai n°3
-
-
-
-
-// Index recettes
-
-const recettes = ["falafel", "fricassé", "tiramisu"];
-
-
-
-const reponse = recettes.filter(recommandation => recommandation.value == "search");
-
-
-
-//                                                  Essai final (exo) ? 
-
-
-
-// Index recettes
-
-let recettes = ["falafel", "fricassé", "tiramisu"];
+let recettes = ["gozleme", "falafel", "fricassé", "paris brest", "tieb", "tiramisu"];
 
 // Récupération du mot recherché
 
@@ -124,23 +155,25 @@ search.addEventListener("keydown", function (event) {
     let reponse;
 
     //Detection de la touche entrée
-   
+    if (event.key === "Enter") {
         //Parcours le tableau recettes grace à for of
+        for (const recette of recettes) {
             //On compare la recherche avec les différents éléments du tableau
-
+            if (recette == recherche) {
                 //On redirige vers la page recherchée si trouvée
-
+                document.location.assign("recette " + recette + ".html");
                 //On passe reponse à true pour dire que c'est bon
-                
+                reponse = true;
                 //On break pour casser la boucle for, au cas où on a plusieurs éléments dans le tableau après le resultat trouvé
-               
-        //On check enfin si la reponse est false pour renvoyer vers 404
-        
-    
+                break;
+            }
+        }
+        //On check si la reponse false pour renvoyer vers 404
+        if (!reponse) {
+            document.location.href = "404.html";
+        }
+    }
 })
-
-
-
 
 
 
